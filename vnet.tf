3a31aa222700277df673b277b0b344e5252df1e4
ryanpray <ryanray@alumni.nd.edu>
2020-05-12 17:03:47 +0000

    fixed code

--- vnet.tf
+++ vnet.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "test" {
 }
 
 module "network" {
-  source              = "https://github.com/Azure/terraform-azurerm-vnet"
+  source              = "https://github.com/ryanpray/terraform-azurerm-vnet"
   resource_group_name = azurerm_resource_group.test.name
   address_space       = "10.0.0.0/16"
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
